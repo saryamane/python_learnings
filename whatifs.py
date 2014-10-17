81886ab54d84674847e5e1cf8e645e4148f56f2e
SamirAryamane <saryamane@salesforce.com>
2014-10-16 21:54:40 -0700

    Another commit for elifs

--- whatifs.py
+++ whatifs.py
@@ -27,3 +27,38 @@ if people <= dogs:
 if people == dogs:
 	print "People are dogs."
 
+
+print
+
+print "Now moving on some car if-else loops"
+
+people = 3
+cars = 40
+trucks = 15
+
+# If multiple elif blocks are true, then it will run only the first one.
+
+if cars > people:
+	print "We should take cars."
+elif cars < people:
+	print "We should not take the cars."
+else:
+	print "We can't decide."
+
+if trucks > cars:
+	print "That's too many trucks."
+elif trucks < cars:
+	print "Maybe we could take the trucks."
+else:
+	print "We still can't decide."
+
+if people > trucks:
+	print "Alright, let's just take the trucks."
+else:
+	print "Fine, let's stay home then."
+
+if people > trucks and cars > trucks:
+	print "We are really brave to try this."
+else:
+	print "We are doomed."
+
